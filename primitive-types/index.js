f3bb89f4ad67515db76897de8142e0c874a39095
chelloh <machelodhiambo@gmail.com>
2019-12-14 00:07:10 +0300

    Define primitive value types

---
+++ primitive-types/index.js
@@ -0,0 +1,14 @@
+/*
+Primitive /Value Types:
+1. String
+2. Number
+3. Boolean
+4. Undefined
+5. Null 
+*/
+
+let name = 'Machel'; // String Literal
+let age = 30; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined;
+let lastName = null;
