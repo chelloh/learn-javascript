38499aa594df8f2f2d14810772faf2131d126157
chelloh <machelodhiambo@gmail.com>
2019-12-13 23:10:18 +0300

    Define keyword 'let'

--- variables/index.js
+++ variables/index.js
@@ -1,2 +1,11 @@
 let name = 'Machel';
 console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+let firstName = 'Machel';
+let lastName = 'Odhiambo';
