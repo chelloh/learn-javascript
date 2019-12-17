fef3fca6e07b2361d1e9c0db4b7ff0ba134df3af
chelloh <machelodhiambo@gmail.com>
2019-12-17 20:50:13 +0300

    Add a 3rd element in the array selectedColors

--- arrays/index.js
+++ arrays/index.js
@@ -3,3 +3,7 @@ console.log(selectedColors);
 
 // Display the 1st Element in the array
 console.log(selectedColors[0]);
+
+// Add a 3rd Element in the array
+selectedColors[2] = 'green';
+console.log(selectedColors)
