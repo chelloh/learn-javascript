5315402da00213a1f4c194020abaa8ee88d3c2bd
chelloh <machelodhiambo@gmail.com>
2019-12-17 20:54:28 +0300

    Check the length of the array

--- arrays/index.js
+++ arrays/index.js
@@ -7,3 +7,6 @@ console.log(selectedColors[0]);
 // Add a 3rd Element in the array
 selectedColors[2] = 'green';
 console.log(selectedColors)
+
+// Let us check the length of this array
+console.log(selectedColors.length);
